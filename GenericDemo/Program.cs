94771304bca5fadc99333038495097ff2632409f
bharathwaj <b.muthuswamy@bhcc.edu>
2021-02-20 12:20:04 -0500

    Example of generic types
    Demonstrate generic type instantiation with int and string

--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
 
 namespace GenericDemo {
+
+    class GenericDemo<T> { // T will hold the type when this class is instantiated
+        public T value { get; private set; }
+
+        public GenericDemo(T value) {
+            this.value = value;
+        }
+
+        public override string ToString() => $"{typeof(T)} : {this.value}";
+    }
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var obj1 = new GenericDemo<int>(33);
+            var obj2 = new GenericDemo<string>("Hello, world!");
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
         }
     }
 }
